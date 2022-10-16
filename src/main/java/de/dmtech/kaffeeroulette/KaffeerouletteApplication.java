b8784ee7874c43fd7d8712b5beee7e2e22f7bc69
t.brunner <thomas@brunner.coffee>
2022-10-16 15:14:12 +0200

    Projektrahmen erzeugt.

---
+++ src/main/java/de/dmtech/kaffeeroulette/KaffeerouletteApplication.java
@@ -0,0 +1,13 @@
+package de.dmtech.kaffeeroulette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaffeerouletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaffeerouletteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/dmtech/kaffeeroulette/KaffeerouletteApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dmtech.kaffeeroulette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaffeerouletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
